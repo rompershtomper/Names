8cd84066e693991ba15ffc5fc45cb3df88ab30fd
rompershtomper <51242739+rompershtomper@users.noreply.github.com>
2019-05-31 20:25:38 +0300

    Add files via upload

---
+++ HeatmapTask.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Names
+{
+    internal static class HeatmapTask
+    {
+        public static HeatmapData GetBirthsPerDateHeatmap(NameData[] names)
+        {
+            double[,] matrix = new double[30, 12];
+            foreach(var e in names)
+            {
+                if (e.BirthDate.Day!=1)
+                    matrix[e.BirthDate.Day-2, e.BirthDate.Month-1]++;
+            }
+
+
+            string[] days = new string[30];
+            for (int i = 0; i < days.Length; i++)
+                days[i] = (2 + i).ToString();
+
+            string[] months = new string[12];
+            for (int i = 0; i < months.Length; i++)
+                months[i] = (1+i).ToString();
+
+            return new HeatmapData(
+                "Пример карты интенсивностей",
+                matrix, 
+                days, 
+                months);
+        }
+    }
+}
+
+//new double[,] { { 1, 2, 3 }, { 2, 3, 4 }, { 3, 4, 4 }, { 4, 4, 4 } },
+//new[] { "a", "b", "c", "d" }, 
+//new[] { "X", "Y", "Z" });
---
+++ HistogramTask.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace Names
+{
+    internal static class HistogramTask
+    {
+        public static HistogramData GetBirthsPerDayHistogram(NameData[] names, string name)
+        {
+            double[] counter = new double[31];
+            foreach (var e in names)
+            {
+                if (e.Name==name && e.BirthDate.Day!=1)
+                    counter[e.BirthDate.Day-1]++;
+            }
+
+            string[] lable = new string[31];
+            for (int i = 0; i < lable.Length; i++)
+                lable[i] = (i+1).ToString();
+
+            return new HistogramData(
+                string.Format("Рождаемость людей с именем '{0}'", name),
+                lable, 
+                counter);
+        }
+    }
+}
